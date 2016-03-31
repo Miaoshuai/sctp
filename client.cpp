7c500e230cbeaa2b0c6ef046e07394ea361222af
miaoshua <945970809@qq.com>
2016-03-31 14:09:10 +0800

    SCTP客户端的实现

---
+++ client.cpp
@@ -0,0 +1,15 @@
+/*======================================================
+    > File Name: client.cpp
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年03月30日 星期三 23时36分06秒
+ =======================================================*/
+#include "client.h"
+
+int main(int argc,char **argv)
+{
+  SctpClient client;
+  client.start();
+  return 0;
+}
--- client.h
+++ client.h
@@ -13,8 +13,15 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-#define SERVER_PORT 3333
+#define SERVER_PORT 6666
+#define MAXLINE     1024
+
+void sctpstr_cli(FILE *fp,int sock_fd,struct sockaddr *to,socklen_t tolen);
 
 class SctpClient
 {
@@ -36,18 +43,19 @@ class SctpClient
             serverAddr_.sin_family = AF_INET;
             serverAddr_.sin_addr.s_addr = htonl(INADDR_ANY);
             serverAddr_.sin_port = htons(SERVER_PORT);
+            inet_pton(AF_INET,"127.0.0.1",&serverAddr_.sin_addr);
 
             bzero(&events_,sizeof(events_));
             events_.sctp_data_io_event = 1;
             setsockopt(sockFd_,IPPROTO_SCTP,SCTP_EVENTS,&events_,sizeof(events_));
             if(echoToAll_ == 0)
             {
-                sctpstr_cli(stdin,sockFd_,(SA *)&serverAddr_,sizeof(serverAddr_));
+                sctpstr_cli(stdin,sockFd_,(struct sockaddr *)&serverAddr_,sizeof(serverAddr_));
             }
-            else
+            /*else
             {
                 sctpstr_cli_echoall(stdin,sockFd_,(SA *)&serverAddr_,sizeof(serverAddr_));
-            }
+            }*/
             close(sockFd_);
         }
 
@@ -56,3 +64,36 @@ class SctpClient
         struct sctp_event_subscribe events_;
         int echoToAll_;
 };
+
+void sctpstr_cli(FILE *fp,int sock_fd,struct sockaddr *to,socklen_t tolen)
+{
+    struct sockaddr_in peeraddr;
+    struct sctp_sndrcvinfo sri;
+    char sendline[MAXLINE];
+    char recvline[MAXLINE];
+    socklen_t len;
+    int out_sz,rd_sz;
+    int msg_flags;
+
+    bzero(&sri,sizeof(sri));
+    while(fgets(sendline,MAXLINE,fp) != NULL)
+    {
+        if(sendline[0] != '[')
+        {
+            printf("ERROR\n");
+            continue;
+        }
+        sri.sinfo_stream = sendline[1] - '0';
+        out_sz = strlen(sendline);
+
+        //发送消息
+        int count = sctp_sendmsg(sock_fd,sendline,out_sz,to,tolen,0,0,sri.sinfo_stream,0,0);
+        printf("count = %d\n",count);
+        len = sizeof(peeraddr);
+        rd_sz = sctp_recvmsg(sock_fd,recvline,sizeof(recvline),
+                             (struct sockaddr *)&peeraddr,&len,&sri,&msg_flags);
+        printf("From str:%d seq:%d (assoc:0x%x):",
+                sri.sinfo_stream,sri.sinfo_ssn,(u_int)sri.sinfo_assoc_id);
+        printf("%d  %s\n",rd_sz,recvline);
+    }
+}
